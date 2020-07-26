62c21a70ddf12c0f65f4f9ab4e788c454b91eb52
Stefan Haselwanter <stefan.haselwanter@gmail.com>
2020-07-26 10:55:01 +0200

    Show and navigate through questions, Show the score, Create a quiz brain to manage quiz logic, Add app screenshots, Show dialog if quiz has finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +29,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> score = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -32,12 +38,12 @@ class _QuizPageState extends State<QuizPage> {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: <Widget>[
         Expanded(
-          flex: 5,
+          flex: 2,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -48,50 +54,79 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Ink(
+                decoration: ShapeDecoration(
+                  color: Colors.green,
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  iconSize: 48.0,
+                  icon: Text(
+                    'True',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () => checkAnswer(true),
                 ),
               ),
-              onPressed: () {
-                //The user picked true.
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+              Ink(
+                decoration: ShapeDecoration(
+                  color: Colors.red,
+                  shape: CircleBorder(),
+                ),
+                child: IconButton(
+                  iconSize: 48.0,
+                  icon: Text(
+                    'False',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () => checkAnswer(false),
                 ),
               ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
+            ],
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: score,
+        )
       ],
     );
   }
-}
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
+  void checkAnswer(bool userAnswer) {
+    setState(() {
+      // Create score icon depending on the answer picked by the user.
+      if (quizBrain.checkAnswer(userAnswer)) {
+        score.add(Icon(Icons.check, color: Colors.green));
+      } else {
+        score.add(Icon(Icons.close, color: Colors.red));
+      }
+      // Check if the quiz has already reached the final question and show an alert.
+      if (quizBrain.isFinished()) {
+        Alert(
+          context: context,
+          title: "Congrats!",
+          desc:
+              "You have finished the quiz and you answered ${getNumberOfCorrectAnswers()} out of ${score.length} questions correctly.",
+        ).show();
+        // Reset the quiz and clear the score immediately.
+        quizBrain.reset();
+        score.clear();
+      } else {
+        // Otherwise, go to the next question.
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
+  int getNumberOfCorrectAnswers() => score.fold(
+      0, (prev, element) => element.icon == Icons.check ? prev + 1 : prev);
+}
